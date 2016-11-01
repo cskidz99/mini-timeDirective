fd20826248d9ca546d622ed2f151b0f0d0fd00bb
Cameron Skidmore <cskidz99@Camerons-MacBook-Air.local>
2016-11-01 13:30:51 -0600

    Explored a few other scope options

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -1,10 +1,17 @@
 angular.module('timeApp').directive('showTime',function(){
   return {
     restrict: 'E',
-    template: '<div> The current time is {{time}} </div>',
-    link: function($scope,element,attrs){
-      var currentTime = new Date();
-      $scope.time = currentTime.toString();
+    template: '<div>Hello {{title}} {{dName}}. The current time is {{time}} </div>',
+    scope: {
+      dName: "=aname",
+      title: "@title"
     },
+    link: function(scope,element,attrs){
+      var currentTime = new Date();
+      scope.time = currentTime.toString();
+    },
+    controller: function($scope){
+
+    }
   }
 })
